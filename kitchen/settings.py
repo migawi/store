c6e7aff32ea8ac16726fd6917ed67c6f688eb162
Mazdon <mazdon@localhost.localdomain>
2022-12-29 15:40:26 +0300

    first commit

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static/')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
